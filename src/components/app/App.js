079ccd75031ec7dc15e8b1442b563d64b0c96c0a
TimAbuev <tim-r10@yandex.ru>
2023-09-18 00:41:06 +0300

    feat: move function from App to Tasks

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,17 +5,6 @@ import Tasks from '../tasks/Tasks';
 
 function App() {
 
-  function charConcat(str) {
-    var i = 1;
-    var res = '';
-    while (str.length > 1) {
-      res += str[0] + str[str.length - 1] + i;
-      i++;
-      str = str.slice(1, -1)
-    }
-    return res;
-  }
-
   return (
     <div className='app'>
       <Routes>
--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -1,9 +1,21 @@
 import './Tasks.css';
 
+function charConcat(str) {
+  var i = 1;
+  var res = '';
+  while (str.length > 1) {
+    res += str[0] + str[str.length - 1] + i;
+    i++;
+    str = str.slice(1, -1)
+  }
+  return res;
+}
+
 function Tasks() {
   return (
     <section className="tasks">
       <p>Здесь будут задания</p>
+      <p>Резульат работы функции charConcat: {charConcat('honda')}</p>
     </section>
   );
 }
