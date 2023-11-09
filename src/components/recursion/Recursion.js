98bf01b0a884ba37ec1d3465d5c84a05c1998f45
TimAbuev <tim-r10@yandex.ru>
2023-11-09 20:41:21 +0300

    feat: add showAllChildren Task to Recursion component

--- src/components/recursion/Recursion.js
+++ src/components/recursion/Recursion.js
@@ -1,4 +1,5 @@
 import './Recursion.css';
+import React from 'react';
 
 // function f1(n) {
 //   let result = 1;
@@ -17,9 +18,11 @@ function f1(n) {
 }
 
 function f2() {
-  let out = '$';
-  for (let i = 1; i <= 9; i++) {
-    out = out + i;
+  let out = '';
+  for (let i = 1; i <= 200; i++) {
+    // out += i + ' ';
+    out = out + i + ' ';
+
     // '$' = '$' + 1;
     // '$1' = '$1' + 2;
     // '$12' = '$12' + 3;
@@ -39,43 +42,112 @@ function f3() {
   }
   return out;
 }
-
+// урок из
 // https://www.youtube.com/watch?v=bh5TKVync9M
+
+// задача с подсчётом денег
 
 function randomInteger(min, max) {
   let rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
 
+let sum = 0;
+
 // Рекурсивное решение
 
-let s1 = 0;
 function moneyRecursion() {
+  if (sum >= 300) return;
   let m = randomInteger(0, 100);
   console.log('add: ' + m);
-  s1 += m;
-  console.log('sum: ' + s1);
-  if (s1 >= 300) return;
+  sum += m;
+  console.log('sum: ' + sum);
   moneyRecursion();
 }
 
 // Циклическое решение
 
 function moneyCycle() {
-  let sum = 0;
   while (true) {
+    if (sum >= 300) return;
     let m = randomInteger(0, 100);
     console.log('add: ' + m);
     sum += m;
     console.log('add: ' + sum);
-    if (sum >= 300) return;
   }
 }
 
+function showSmth() {
+  moneyCycle();
+  return sum;
+}
+
+
+
 function Recursion() {
+  // задача с выводом дочерних элементов
+
+  const [array, setArray] = React.useState([]);
+  const newArray = [];
+  newArray.split(' ');
+
+  React.useEffect(() => {
+    setArray(newArray);
+  }, []);
+
+  React.useEffect(() => {
+    for (let key in users) {
+      userParentRecursion(users[key]);
+    }
+  }, []);
+
+  const users = {
+    "ivanov": {
+      age: 25,
+      favColor: 'red',
+      parent: {
+        "ivanov-b": {
+          age: 43,
+          favColor: 'white',
+          parent: {
+            "sergeev-a": {
+              age: 88,
+              parent: {
+                "lionenko": {}
+              }
+            }
+          }
+        }
+      }
+    },
+    "kostenko": {
+      age: 56,
+      parent: {
+        "ignatenko": {
+
+        },
+        "sniezko": {
+          age: 45
+        }
+      }
+    }
+  }
+
+  function userParentRecursion(obj) {
+
+    if (obj.parent !== undefined) {
+      for (let key in obj.parent) {
+        console.log(key);
+        newArray.push(key);
+        userParentRecursion(obj.parent[key]);
+      }
+    }
+  }
+
   return (
     <section className="experience">
-      <p className='experience__paragraph'>Результат работы функции: {moneyCycle()}</p>
+      <p className='experience__paragraph'>(тема рекурсия)array: {array}</p>
+      {/* <p className='experience__paragraph'>Результат работы функции: {array}</p> */}
     </section>
   );
 }
